f7846a835bbc9a195bce79cb94a4aaabf21cb825
Monday_David_Sarka <david_md@hamari.agency>
2021-12-08 01:31:59 +0100

    passing a callback

--- src/App.js
+++ src/App.js
@@ -1,33 +1,60 @@
 import React from 'react';
 import './App.css';
+
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: "Davido"
+      inputValue: ''
     }
+    this.handleChange = this.handleChange.bind(this);
   }
-
+  handleChange(event) {
+    this.setState({
+      inputValue: event.target.value
+    });
+  }
   render() {
     return (
-      <div>
-        <NavBar name={this.state.name}/>
-      </div>
-    )
+       <div>
+        { /* Change code below this line */ }
+    <GetInput input={this.state.inputValue}
+    handleChange={this.handleChange}/>
+    <RenderInput input={this.state.inputValue} />
+        { /* Change code above this line */ }
+       </div>
+    );
   }
-}
+};
 
-class NavBar extends React.Component {
-  constructor (props) {
+class GetInput extends React.Component {
+  constructor(props) {
     super(props);
   }
   render() {
     return (
       <div>
-        <h1>Welcome Monsior  {this.props.name}</h1>
+        <h3>Get Input:</h3>
+        <input
+          value={this.props.input}
+          onChange={this.props.handleChange}/>
       </div>
-    )
+    );
   }
-}
+};
+
+class RenderInput extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h3>Input Render:</h3>
+        <p>{this.props.input}</p>
+      </div>
+    );
+  }
+};
 
 export default App;
